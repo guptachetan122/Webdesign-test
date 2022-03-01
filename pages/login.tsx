ab35e55098981254751be3c706d0a6d912800822
Chetan Gupta <guptachetan122@gmail.com>
2022-03-01 18:28:59 +0530

    logo photo

--- pages/login.tsx
+++ pages/login.tsx
@@ -15,7 +15,7 @@ const oldLogin = () => {
 
     <Row  className={styles.mainSectionWrapper}>
         <Col xs={0} sm={0} md={5} lg={6} xl={7} xxl={7} ></Col>
-        <Col  xs={24} sm ={24} md={14} lg={12} xl={10} xxl={10} >
+        <Col  xs={24} sm ={24} md={14} lg={12} xl={10} xxl={10} className={styles.collog}>
             <img className={styles.imageWrapper}
 							src='../assets/images/image3.png'
 							alt='leap club logo'
